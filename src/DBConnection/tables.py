fb2e20b27e1f4e8a6b87803ad8f4981c5c0b615c
Carsten Vogel <carsten.vogel@uni-wuerzburg.de>
2022-01-20 16:45:25 +0100

    added: Tables -> Get All Answers

--- src/DBConnection/tables.py
+++ src/DBConnection/tables.py
@@ -8,10 +8,23 @@ def test():
 def get_all_users():
   sql_query = "SELECT * FROM users"
 
-  users = dbConnection.run(sql_query)
+  result = dbConnection.run(sql_query)
 
-  return users
+  return result
+
+def get_all_answers():
+  sql_query = "SELECT * FROM answers"
+
+  result = dbConnection.run(sql_query)
+
+  return result
 
 # users = get_all_users()
+# print('users - BEGIN')
 # print('users.head():', '\t', users.head())
-# print('TEST END')
+# print('users - END')
+
+# answers = get_all_answers()
+# print('answers - BEGIN')
+# print('answers.head():', '\t', answers.head())
+# print('answers - END')
